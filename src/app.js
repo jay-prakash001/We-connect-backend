8fcfec2da66ddcd26bc14b8fd93f7ce445d12abb
jay prakash <iamjayprakashdewangan2004@gmail.com>
2024-11-22 08:26:59 +0530

    otp verification code running correctly

--- src/app.js
+++ src/app.js
@@ -19,33 +19,37 @@ app.post('/create_post', async (req, res)=>{
     console.log(post)
     res.send(post)
 })
-const otpStore = new Map()
 
-app.post('/get_otp',(req, res)=>{ 
-const {client_phone} = req.body
+const generate = ()=> {
+    return Math.floor(100000 + Math.random() * 900000).toString();
+}
+const otpStore = new Map();
 
-if(!client_phone){
-    return res.status(400).json({error:"invalid phone number"})
-}
-const otp =  Math.floor(100000 + Math.random()* 900000)
-console.log(otp)
-otpStore.set(client_phone,otp)
-console.log(otpStore)
-try {
-    client.messages.create({
-        to : "+91" + client_phone,
-        from : phone,
-        body : "Your otp for " + client_phone  + " is " + otp
-    })
-} catch (error) {
-    console.log(error)
-}
-res.end()
+app.post('/getotp', (req, res) => {
+    const { client_phone } = req.body
+
+    if (!client_phone) {
+        return res.status(400).json({ error: "Phone number is required." })
+    }
+    const otp = generate()
+    otpStore.set(client_phone, otp)
+    console.log(otpStore)
+    try {
+        const message = client.messages.create({
+            to: "+91"+client_phone,
+            from: phone,
+            body: "Your Otp for " + client_phone + " is " + otp
+        })
+        console.log("message id : " + message.sid)
+    } catch (error) { 
+        res.write(error)
+    }
+    res.end()
 })
 
 
 
-app.post("/verify_otp", (req, res) => {
+app.post("/verifyotp", (req, res) => {
     const { client_phone, otp } = req.body; // Assuming phone and OTP are provided in the request body
     console.log(otpStore)
 
